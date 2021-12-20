be1bb58f52de9436ae693b56e077039c5a9d45e3
shubhamdixit <mobcoderid-170@Mobcoders-MacBook-Pro-3.local>
2021-12-20 15:29:31 +0530

    Formatting error messages

--- pkg/converter/convert.go
+++ pkg/converter/convert.go
@@ -1,6 +1,7 @@
 package converter
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -12,12 +13,13 @@ import (
 func Convert(inputFiles []string, outputDir string) error {
 	schemas, err := inputs.ReadInputFiles(inputFiles, true)
 	if err != nil {
-		return err
+		return fmt.Errorf("error while reading input file %s", err)
+
 	}
 	generatorInstance := inputs.New(schemas...) // instance of generator which will produce structs
 	err = generatorInstance.CreateTypes()
 	if err != nil {
-		return err
+		return fmt.Errorf("error while generating instance for  proudcing structs %s", err)
 	}
 
 	for _, file := range inputFiles {
@@ -25,12 +27,13 @@ func Convert(inputFiles []string, outputDir string) error {
 		packageDirectory, packageName := utils.PackageFormat(outputDir, file)
 		err = os.Mkdir(packageDirectory, 0755)
 		if err != nil {
-			return err
+			return fmt.Errorf("error while creating directory %s", err)
+
 		}
 		w, err = os.Create(filepath.Join(packageDirectory, filepath.Base(utils.FileNameCreation(file))))
 
 		if err != nil {
-			return err
+			return fmt.Errorf("error while generating Files %s", err)
 		}
 		// Model Generation Method Called
 		inputs.Output(w, generatorInstance, packageName)
